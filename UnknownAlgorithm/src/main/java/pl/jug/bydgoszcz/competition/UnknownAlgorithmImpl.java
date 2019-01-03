23e33a81d563d2de462d19d0d6b3dee92edc74a9
inodzik <inodzik@gmail.com>
2019-01-03 19:55:25 +0100

    UnknownAlgorithm: initial version

---
+++ UnknownAlgorithm/src/main/java/pl/jug/bydgoszcz/competition/UnknownAlgorithmImpl.java
@@ -0,0 +1,22 @@
+package pl.jug.bydgoszcz.competition;
+
+import java.util.List;
+
+public class UnknownAlgorithmImpl {
+    public static List<Integer> doCalculation(List<Integer> input) {
+        // find buckets number
+        int max = 0;
+        for (int value : input) if (max < value) max = value;
+
+        // create buckets
+        int counter[] = new int[max + 1];
+
+        // fill buckets
+        for (int i : input) counter[i]++;
+
+        // do something
+        for (int idx = 0, i = 0; i < counter.length; i++) while (0 < counter[i]--) input.set(idx++, i);
+
+        return input;
+    }
+}
